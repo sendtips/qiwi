b2b0c84a39fcf9fe7077b26cc0f1b5f3642b0e65
Igor A. Melekhine <igor.melekhine@me.com>
2021-10-29 15:00:29 +0000

    QIwi time instead time.Time

--- card.go
+++ card.go
@@ -23,7 +23,7 @@ const (
 
 // Card holds data for card payment
 type Card struct {
-	CheckDate  time.Time  `json:"checkOperationDate"`      // System date of the operation
+	CheckDate  QIWITime   `json:"checkOperationDate"`      // System date of the operation
 	RequestID  string     `json:"requestUid"`              // Card verification operation unique identifier String(200)
 	Status     Status     `json:"status"`                  // Card verification status	String
 	Valid      bool       `json:"isValidCard"`             // Logical flag means card is valid for purchases Bool
@@ -52,10 +52,10 @@ type CardInfo struct {
 
 // CardToken shadowed card
 type CardToken struct {
-	Token          string    `json:"token"`       // Card payment token	String
-	Name           string    `json:"name"`        // Masked card PAN for which payment token issued	String
-	ExpirationDate time.Time `json:"expiredDate"` // Payment token expiration date. ISO-8601
-	Account        string    `json:"account"`     // Customer account for which payment token issued	String
+	Token          string   `json:"token"`       // Card payment token	String
+	Name           string   `json:"name"`        // Masked card PAN for which payment token issued	String
+	ExpirationDate QIWITime `json:"expiredDate"` // Payment token expiration date. ISO-8601
+	Account        string   `json:"account"`     // Customer account for which payment token issued	String
 }
 
 // CardRequest request payment session on RSP site
