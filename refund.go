babbc719b2752da0e85dd422c7e9456f4dd150c3
Igor A. Melekhine <igor.melekhine@me.com>
2023-08-08 13:26:56 +0300

    lint

--- refund.go
+++ refund.go
@@ -1,11 +1,12 @@
 package qiwi
 
+// RefundSplits contains split refund.
 type RefundSplits struct {
 	Split
 	Commission *RefundSplitCommission `json:"splitCommissions,omitempty"`
 }
 
-// RefundSplitCommission contains commission information
+// RefundSplitCommission contains commission information.
 type RefundSplitCommission struct {
 	Amount  *Amount `json:"merchantCms,omitempty"`
 	UserCms string  `json:"userCms,omitempty"`
